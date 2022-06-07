33d43a49838070230b67c054668cd1feab40ebe8
Ehsaas <guptaehsaas7@gmail.com>
2022-06-07 22:55:00 +0530

    Program for conversion of Inches to Centimeter.

---
+++ 11_Inches_to_Centimeter.py
@@ -0,0 +1,6 @@
+# Inches to Centimeter
+Inch = int(input("Enter the value of Inch\n"))
+def ItoC():
+    return Inch*2.54
+Centimeter = ItoC()
+print(Inch,"inches is equal to",Centimeter,"centimeters")
